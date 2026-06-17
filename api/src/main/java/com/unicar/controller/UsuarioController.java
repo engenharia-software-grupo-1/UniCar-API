63c19498644b162d9154309d1bf52892e2ab2123
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2026-06-17 00:01:44 -0300

    feat: adiciona estrutura do projeto com domain base usuario

---
+++ api/src/main/java/com/unicar/controller/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.unicar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @GetMapping
+    public String listar() {
+        return "Usuários funcionando";
+    }
+}
