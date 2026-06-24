bb41cc49aa5e3a04e6153fb8226901476fb21a66
Oscar Rodrigues <ukboscar@gmail.com>
2026-06-23 21:44:02 -0300

    feat: adiciona service

--- api/src/main/java/com/unicar/service/UsuarioService.java
+++ api/src/main/java/com/unicar/service/UsuarioService.java
@@ -1,7 +1,57 @@
 package com.unicar.service;
 
+import com.unicar.domain.Usuario;
+import com.unicar.dto.UpdatePerfilRequestDTO;
+import com.unicar.dto.UsuarioLogadoResponseDTO;
+import com.unicar.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioLogadoResponseDTO buscarPerfil(Long id) {
+
+        Usuario usuario = buscarUsuario(id);
+
+        return UsuarioLogadoResponseDTO.from(usuario);
+    }
+
+    public UsuarioLogadoResponseDTO atualizarPerfil(
+            Long id,
+            UpdatePerfilRequestDTO request
+    ) {
+
+        Usuario usuario = buscarUsuario(id);
+
+        if (request.genero() != null) {
+            usuario.setGenero(request.genero());
+        }
+
+        if (request.receberEmail() != null) {
+            usuario.setReceberEmail(request.receberEmail());
+        }
+
+        usuarioRepository.save(usuario);
+
+        return UsuarioLogadoResponseDTO.from(usuario);
+    }
+
+    public void desativarPerfil(Long id) {
+
+        Usuario usuario = buscarUsuario(id);
+
+        usuario.setAtivo(false);
+
+        usuarioRepository.save(usuario);
+    }
+
+    private Usuario buscarUsuario(Long id) {
+
+        return usuarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
 }
