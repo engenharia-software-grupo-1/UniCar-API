86f3d6fc1774d65d57443ebde60028ab748e3924
Isadora Lucena <isadorabeatriz595@gmail.com>
2026-07-09 21:23:52 -0300

    refactor: cria exception customizada para veiculo nao encontrado

---
+++ api/src/main/java/com/unicar/exception/VeiculoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.unicar.exception;
+
+public class VeiculoNaoEncontradoException extends RuntimeException {
+    public VeiculoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
