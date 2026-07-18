d80261efe64169296b3e76b7e640a56bc6001495
Oscar Rodrigues <ukboscar@gmail.com>
2026-07-18 16:38:31 -0300

    feat: adiciona parte de enviar email

--- api/src/main/java/com/unicar/service/EmailService.java
+++ api/src/main/java/com/unicar/service/EmailService.java
@@ -1,4 +1,28 @@
 package com.unicar.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Async
+    public void enviarEmail(String para, String assunto, String corpo) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(para);
+            message.setSubject(assunto);
+            message.setText(corpo);
+            mailSender.send(message);
+        } catch (Exception e) {
+
+            System.err.println("Falha ao enviar e-mail para " + para + ": " + e.getMessage());
+        }
+    }
 }
